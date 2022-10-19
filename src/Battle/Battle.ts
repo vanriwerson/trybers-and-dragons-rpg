43c9a3d84f080abf159eeaa461b46b6fc93db30b
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-19 08:16:12 -0300

    fix: accepting type Fighter and Character on Battle constructor

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,7 +1,8 @@
+import Character from '../Character';
 import Fighter from '../Fighter';
 
 abstract class Battle {
-  constructor(protected player: Fighter) { }
+  constructor(protected player: Fighter | Character) { }
 
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
