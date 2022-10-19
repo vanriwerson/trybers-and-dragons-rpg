d15af4c3dbfe1f9edd99781f58eaf0c8ac3c6180
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-19 07:50:32 -0300

    fix: including SimpleFighter on attack paramaters types

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -61,7 +61,7 @@ export default class Character implements Fighter {
     return { ...this._energy };
   }
 
-  public attack(enemy: Fighter) {
+  public attack(enemy: Fighter | SimpleFighter) {
     enemy.receiveDamage(this._strength);
   }
 
