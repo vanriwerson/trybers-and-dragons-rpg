e904e427cede74a4d05cfb54eb9307d2ef17cc8b
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-19 07:51:03 -0300

    feat: implements class Monster

---
+++ src/Monster.ts
@@ -0,0 +1,36 @@
+import Character from './Character';
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public attack(enemy: Character) {
+    enemy.receiveDamage(this._strength);
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+
+      if (this._lifePoints <= 0) {
+        this._lifePoints = -1;
+      }
+    }
+
+    return this._lifePoints;
+  }
+}
